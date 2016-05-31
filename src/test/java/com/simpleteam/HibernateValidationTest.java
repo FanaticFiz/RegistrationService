158116fcb5754b54da5a18a4fdb39c2266ddec82
Fiz <fanatic83@gmail.com>
2016-05-31 18:47:42 +0300

    test for validation

---
+++ src/test/java/com/simpleteam/HibernateValidationTest.java
@@ -0,0 +1,53 @@
+package com.simpleteam;
+
+import com.simpleteam.entity.User;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class HibernateValidationTest {
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void passwordTest() {
+        User user = new User();
+        user.setPassword("secretPassword");
+        Set<ConstraintViolation<User>> constraintViolations = validator.validate( user );
+
+        assertEquals(1, constraintViolations.size());
+        assertEquals(
+                "Password must contain at least 2 digits and one \"!\" symbol",
+                constraintViolations.iterator().next().getMessage()
+        );
+    }
+
+    @Test
+    public void emailTest() {
+        User user = new User();
+        user.setEmail("wrongEmail.com");
+        Set<ConstraintViolation<User>> constraintViolationsFalse = validator.validate( user );
+        assertEquals(1, constraintViolationsFalse.size());
+        assertEquals(
+                "not a well-formed email address",
+                constraintViolationsFalse.iterator().next().getMessage()
+        );
+
+        user.setEmail("good@gmail.com");
+        Set<ConstraintViolation<User>> constraintViolationsTrue = validator.validate( user );
+        assertEquals(0, constraintViolationsTrue.size());
+    }
+
+}
